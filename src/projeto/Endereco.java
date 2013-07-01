b40219ee762c4a120beecee2c002d5519fea511e
erivaldo.s.b@gmail.com <erivaldo.s.b@gmail.com>
2013-07-01 13:47:01 -0300

    Primeiro commit Erivaldo

---
+++ src/projeto/Endereco.java
@@ -0,0 +1,42 @@
+package projeto;
+
+public class Endereco {
+
+	private String rua;
+	private String telefone;
+	private String bairro;
+	private String apartamento;
+	private int numeroCasaOuApartamento;
+	public String getRua() {
+		return rua;
+	}
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getBairro() {
+		return bairro;
+	}
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	public String getApartamento() {
+		return apartamento;
+	}
+	public void setApartamento(String apartamento) {
+		this.apartamento = apartamento;
+	}
+	public int getNumeroCasaOuApartamento() {
+		return numeroCasaOuApartamento;
+	}
+	public void setNumeroCasaOuApartamento(int numeroCasaOuApartamento) {
+		this.numeroCasaOuApartamento = numeroCasaOuApartamento;
+	}
+
+	
+}
---
+++ src/projeto/MinhasException.java
@@ -0,0 +1,10 @@
+package projeto;
+
+public class MinhasException extends Exception{
+
+	public MinhasException(){
+		super();
+	}
+	
+
+}
